e768eb2be0ab199d8324d925b83c8e6d854e5492
vagrant <vagrant@vagrant.vm>
2020-10-07 22:38:13 +0000

    next

--- markov.py
+++ markov.py
@@ -2,6 +2,9 @@
 
 import sys
 from random import choice
+import os
+import discord
+
 
 
 def open_and_read_file(filenames):
@@ -14,6 +17,9 @@ def open_and_read_file(filenames):
         text_file.close()
 
     return body
+    
+  
+
 
 
 def make_chains(text_string):
@@ -64,3 +70,37 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+        await message.channel.send(make_text(chains))
+
+client.run(os.environ['DISCORD_TOKEN'])
+
+# client = discord.Client()
+
+
+# @client.event
+# async def on_ready():
+#     print(f'Successfully connected! Logged in as {client.user}.')
+
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     await message.channel.send(make_text(chains))
+
+
+# client.run(os.environ['DISCORD_TOKEN'])
